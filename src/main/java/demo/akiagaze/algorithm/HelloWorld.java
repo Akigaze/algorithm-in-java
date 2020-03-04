26aaee4bec39e9a38ba633ea6934d406de101920
Akigaze <1175520196@qq.com>
2020-03-04 22:20:23 +0800

    init project

---
+++ src/main/java/demo/akiagaze/algorithm/HelloWorld.java
@@ -0,0 +1,7 @@
+package demo.akiagaze.algorithm;
+
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("this is a hello world.");
+  }
+}
---
+++ src/test/java/demo/akiagaze/algorithm/HelloWorldTest.java
@@ -0,0 +1,12 @@
+package demo.akiagaze.algorithm;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HelloWorldTest {
+  @Test
+  public void should_be_a_hello_world() {
+    assertEquals("hello world", "hello world");
+  }
+}
